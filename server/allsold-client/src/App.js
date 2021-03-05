b28ad30aeda5f547aa14ceb8515dff9ce31f80ed
mayank8903 <mayankraj2608@gmail.com>
2021-03-04 22:07:57 -0500

    starting header

--- server/allsold-client/src/App.js
+++ server/allsold-client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <h1>Start of project</h1>
+      <Header />
     </div>
   );
 }
