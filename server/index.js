d65b6243353f534d04b1a98283beea3a638cae26
Abhishek jindal <zeus1997aj@users.noreply.github.com>
2021-03-04 23:40:52 -0500

    Created middlewares

--- server/index.js
+++ server/index.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const Mongoose = require("mongoose");
 
+//Environment Variables
+require("dotenv").config();
+
+const middlewares = require("./middlewares");
+
+// app config
 const app = express();
 
+//connect to mongoose
+const OPTS = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+Mongoose.connect(process.env.DATABASE_URL, OPTS)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+// api routes
 app.get("/", (req, res) => {
   res.send({ start: "backend" });
 });
 
-const PORT = 5000;
+//middlewares
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
-app.listen(5000);
+// port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port} \n Server up and running`);
+});
