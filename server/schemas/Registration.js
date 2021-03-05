e8f6f596f117e78ae03c6c0fb364dd4f06c9b411
Abhishek jindal <zeus1997aj@users.noreply.github.com>
2021-03-05 00:20:33 -0500

    Crated registration Schema

---
+++ server/schemas/Registration.js
@@ -0,0 +1,17 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema;
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const RegistrationSchema = new Schema({
+  FirstName: requiredString,
+  LastName: requiredString,
+  Email: { type: String, required: true, unique: true },
+  Password: requiredString,
+});
+
+const Registration = Mongoose.model("Registration", RegistrationSchema); // model gives us methods like find,create
+module.exports = Registration;
