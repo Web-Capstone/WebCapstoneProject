106ee0f17fd105fda153ea36afb96068c8a6802e
mayank8903 <mayankraj2608@gmail.com>
2021-04-08 15:50:46 -0400

    added pricing module

--- server/allsold-client/src/components/Home.js
+++ server/allsold-client/src/components/Home.js
@@ -7,13 +7,15 @@ import {
   Card,
   CardContent,
   CardActions,
+  CardHeader,
 } from "@material-ui/core";
 import "../styles/home.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-
+import StarIcon from "@material-ui/icons/StarBorder";
+import purple from "@material-ui/core/colors/purple";
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -77,9 +79,75 @@ const useStyles = makeStyles((theme) => ({
   cardContent: {
     flexGrow: 1,
   },
+
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+  priceCard: {
+    marginTop: theme.spacing(4),
+  },
+
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  // cssRoot: {
+  //   color: theme.palette.getContrastText(purple[500]),
+  //   backgroundColor: purple[500],
+  //   "&:hover": {
+  //     backgroundColor: purple[700],
+  //   },
+  // },
 }));
 
 const cards = [1, 2, 3, 4, 5, 6];
+const tiers = [
+  {
+    title: "Free",
+    price: "0",
+    description: [
+      "10 ads per month",
+      "2 active ads ",
+      "Help center access",
+      "Email support",
+    ],
+    buttonText: "Sign up for free",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "ProSeller",
+    subheader: "Most popular",
+    price: "15",
+    description: [
+      "20 ads per month",
+      "10 active ads",
+      "Help center access",
+      "Priority email support",
+    ],
+    buttonText: "Get started",
+    buttonVariant: "contained",
+  },
+  {
+    title: "Corporate Seller",
+    price: "30",
+    description: [
+      "50 ads per month",
+      "30 active ads",
+      "Help center access",
+      "Phone & email support",
+    ],
+    buttonText: "Contact us",
+    buttonVariant: "outlined",
+  },
+];
 
 function Home() {
   const classes = useStyles();
@@ -135,6 +203,91 @@ function Home() {
                     </Button>
                     <Button size="small" color="secondary">
                       Favorite
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        <Container
+          maxWidth="sm"
+          component="main"
+          className={classes.heroContent}
+        >
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Pricing
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Now get most out of your advertisements by picking our plans that
+            are perfectly curated for all types of individuals
+          </Typography>
+        </Container>
+
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+            {tiers.map((tier) => (
+              <Grid
+                item
+                key={tier.title}
+                xs={12}
+                sm={tier.title === "Enterprise" ? 12 : 6}
+                md={4}
+              >
+                <Card className={classes.priceCard}>
+                  <CardHeader
+                    title={tier.title}
+                    subheader={tier.subheader}
+                    titleTypographyProps={{ align: "center" }}
+                    subheaderTypographyProps={{ align: "center" }}
+                    action={tier.title === "ProSeller" ? <StarIcon /> : null}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.cardPricing}>
+                      <Typography
+                        component="h2"
+                        variant="h3"
+                        color="textPrimary"
+                      >
+                        ${tier.price}
+                      </Typography>
+                      <Typography variant="h6" color="textSecondary">
+                        /mo
+                      </Typography>
+                    </div>
+                    <ul>
+                      {tier.description.map((line) => (
+                        <Typography
+                          component="li"
+                          variant="subtitle1"
+                          align="center"
+                          key={line}
+                        >
+                          {line}
+                        </Typography>
+                      ))}
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      fullWidth
+                      variant={tier.buttonVariant}
+                      color="secondary"
+                      // className={classes.cssRoot}
+                    >
+                      {tier.buttonText}
                     </Button>
                   </CardActions>
                 </Card>
