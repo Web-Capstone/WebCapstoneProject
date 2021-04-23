154aaab5aa7af12d6ae1eebb98f0e8cd78337b6c
mayank8903 <mayankraj2608@gmail.com>
2021-04-22 21:49:31 -0400

    refactoring app.js

--- server/allsold-client/src/App.js
+++ server/allsold-client/src/App.js
@@ -4,8 +4,6 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import { useEffect } from "react";
-import { connect } from "react-redux";
-import * as actions from "./actions";
 import Register from "./components/Register.js";
 import Products from "./components/Products";
 import ProductDetails from "./components/productDetails";
@@ -13,16 +11,20 @@ import { useDispatch } from "react-redux";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./styles/UniversalTheme";
 
-import { getAllProducts, getExtraProducts } from "./actions/index";
+import {
+  getAllProducts,
+  getExtraProducts,
+  fetchGoogleUser,
+} from "./actions/index";
 
 import PostAdd from "./components/PostAdd";
 
-function App(props) {
+function App() {
   const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    props.fetchGoogleUser();
+    dispatch(fetchGoogleUser());
     dispatch(getAllProducts());
     dispatch(getExtraProducts());
   }, []);
@@ -32,19 +34,16 @@ function App(props) {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <Header />
-          {/* <Container maxwidth="md"> */}
           <Route path="/PostAdd" component={PostAdd} />
           <Route path="/Login" component={Login} />
           <Route path="/Register" component={Register} />
           <Route path="/products" component={Products} />
           <Route path="/productDetails/:id" component={ProductDetails} />
           <Route path="/" exact component={Home} />
-
-          {/* </Container> */}
         </ThemeProvider>
       </BrowserRouter>
     </div>
   );
 }
 
-export default connect(null, actions)(App);
+export default App;
